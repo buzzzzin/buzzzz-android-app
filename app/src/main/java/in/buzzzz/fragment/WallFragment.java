d396e239c7b3682f7d5c51e7d69bee0fa1dad5b9
navkrishna <navkrishna@intelligrape.com>
2015-10-24 22:57:25 +0530

    created wall fragment

---
+++ app/src/main/java/in/buzzzz/fragment/WallFragment.java
@@ -0,0 +1,23 @@
+package in.buzzzz.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import in.buzzzz.R;
+
+/**
+ * Created by Navkrishna on October 24, 2015
+ */
+public class WallFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_wall, container, false);
+        return view;
+    }
+}
